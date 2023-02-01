11bf79f7cbd6c506348e801c17c803023db4a788
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2023-02-01 13:48:26 +0100

    moving index.jsx into the lib folder

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Modal from "./src/lib/components/Modal/Modal";
+import Modal from "./components/Modal/Modal";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
