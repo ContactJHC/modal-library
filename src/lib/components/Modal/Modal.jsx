01699fd6b25095f4301ed75bd37f94f942fc719e
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2023-02-17 15:19:52 +0100

    penser aux doublons

--- src/lib/components/Modal/Modal.jsx
+++ src/lib/components/Modal/Modal.jsx
@@ -2,29 +2,6 @@ import React from 'react';
 import { useCallback, useEffect } from 'react';
 import './Modal.css'
 import closeIcon from '../assets/close-icon.svg'
-
-/**
- * Returns a modal component with props which are explained below:
- * the width of the modal is managed via the width attribute of the style prop or the large prop or the small prop
- * - large : boolean - true or false
- * - small : boolean - true or false
- * - style : object - written in React Inline Css Style according to the schema below
- * {
-    width : '',
-    backgroundColor: '',
-    borderRadius: '',
-    color: '',
-    fontSize: ''
-  }
- * - onClose : function - the closing behavior or the modal
- * - show : boolean - with regards to the local state of [show, setShow] if the modal is visible or not
- * - the modal content is defined bu the children prop directly in the <Modal> </Modal> component
- *
- */
-  import React from 'react';
-  import { useCallback, useEffect } from 'react'
-  import './Modal.css'
-  import closeIcon from '../../assets/close-icon.svg'
   
   /**
    * Returns a modal component with props which are explained below:
